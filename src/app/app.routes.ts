f89669e35142677effab79fc31b0ab0cfe527fe2
Victor Garcia <victor.garcia.florez@gmail.com>
2025-06-27 12:34:25 +0200

    Created some components and updated routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    // No tiene "export default" en reactive.routes.ts
+    loadChildren: () => import('./reactive/reactive.routes').then((module) => module.reactiveRoutes),
+  },
+  {
+    path: 'auth',
+    // Sí tiene "export default" en auth.routes.ts
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: 'country',
+    // No tiene "export default" en reactive.routes.ts
+    loadChildren: () => import('./country/country.routes').then((module) => module.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  },
+];
--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuItem {
+  title: string;
+  route: string;
+};
+
+// Siempre exista una ruta y no caer en el undefine.
+const reactiveItems = reactiveRoutes[0].children ?? [];
+
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+  }));
+
+  authMenu: MenuItem[] = [{
+    title: 'Registro',
+    route: './auth',
+  }];
+
+  countryMenu: MenuItem[] = [{
+    title: 'Países',
+    route: './country',
+  }];
+}
