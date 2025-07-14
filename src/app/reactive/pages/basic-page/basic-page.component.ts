1539d1b69da7209cab6d652d8e997fc695f63537
Victor Garcia <victor.garcia.florez@gmail.com>
2025-07-14 11:57:29 +0200

    Show errors on form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -24,10 +24,35 @@ export class BasicPageComponent {
   // Forma recomendada (form builder)
   private fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  isValidField(fieldName: string): boolean | null {
+    return !!this.myForm.controls[fieldName].errors;
+  };
+
+  getFieldError(fieldName: string): string | null {
+    if(!this.myForm.controls[fieldName].errors) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    // Recorrer todas las llaves de la variable errors
+    for(const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${ errors['minlength'].requiredLength } caracteres.`;
+
+        case 'min':
+          return `Valor mínimo de ${ errors['min'].min }`;
+      }
+    }
+
+    return null;
+  }
 }
