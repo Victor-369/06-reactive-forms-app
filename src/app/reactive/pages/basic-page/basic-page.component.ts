de6795fc95b3b958546625c0cfbb037e70803b38
Victor Garcia <victor.garcia.florez@gmail.com>
2025-07-14 11:30:30 +0200

    Examples of validations on inputs from a form.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,12 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     JsonPipe,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './basic-page.component.html',
 })
@@ -14,14 +14,20 @@ export class BasicPageComponent {
   // Formulario reactivo
 
   //Forma tradicional
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  // myForm = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+
+  // Forma recomendada (form builder)
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
-
-
-  // Forma recomendada
-
 
 }
