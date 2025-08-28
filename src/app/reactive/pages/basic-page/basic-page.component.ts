777e0d5a43098a8918c1b4e681808b7e34986e2b
Victor Garcia <victor.garcia.florez@gmail.com>
2025-08-28 14:14:38 +0200

    Update of basic-page component.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -23,6 +24,8 @@ export class BasicPageComponent {
 
   // Forma recomendada (form builder)
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -30,29 +33,21 @@ export class BasicPageComponent {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  isValidField(fieldName: string): boolean | null {
-    return !!this.myForm.controls[fieldName].errors;
-  };
+  onSave() {
+    if(this.myForm.invalid) {         // Si el formulario es inválido
+      this.myForm.markAllAsTouched(); // Toca todos los campos del formulario, así hará saltar las validaciones de todos ellos.
 
-  getFieldError(fieldName: string): string | null {
-    if(!this.myForm.controls[fieldName].errors) return null;
-
-    const errors = this.myForm.controls[fieldName].errors ?? {};
-
-    // Recorrer todas las llaves de la variable errors
-    for(const key of Object.keys(errors)) {
-      switch(key) {
-        case 'required':
-          return 'Este campo es requerido';
-
-        case 'minlength':
-          return `Mínimo de ${ errors['minlength'].requiredLength } caracteres.`;
-
-        case 'min':
-          return `Valor mínimo de ${ errors['min'].min }`;
-      }
+      return;
     }
 
-    return null;
+    console.log(this.myForm.value);
+
+    // this.myForm.reset({
+    //   price: 100,
+    //   inStorage: 50,
+    //   name: 'ValorPorDefecto',
+    // });
+
+    this.myForm.reset();
   }
 }
