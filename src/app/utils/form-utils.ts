37d72bd3d0c8a906a58515734014789689f7d8e5
Victor Garcia <victor.garcia.florez@gmail.com>
2025-10-02 11:53:32 +0200

    Updated register page. Added first version of validation for email.

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -16,6 +16,9 @@ export class FormUtils {
 
         case 'min':
           return `Valor mínimo de ${ errors['min'].min }`;
+
+        case 'email':
+          return `El valor ingresado no es un correo electrónico`;
       }
     }
 
